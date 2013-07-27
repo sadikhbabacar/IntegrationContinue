5cd49c5b644d9b3b90343e387d9b7a02a3387b30
Mbaye Babacar <sadikhbabacarmbaye@gmail.com>
2013-07-27 13:44:23 +0000

    Test sur le projet d'integration Continue

---
+++ Impl_Calculatrice.java
@@ -0,0 +1,29 @@
+package test;
+
+public class Impl_Calculatrice implements Calculatrice {
+
+	@Override
+	public int Addition(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int Soustraction(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int Multiplication(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int Division(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
