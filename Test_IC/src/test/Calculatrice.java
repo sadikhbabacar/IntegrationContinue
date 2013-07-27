35351a8e18df0586d20aa2d36f46a6552c98da75
Mbaye Babacar <sadikhbabacarmbaye@gmail.com>
2013-07-27 19:12:25 +0000

    Test Projet Integration Continue

---
+++ Test_IC/src/test/Calculatrice.java
@@ -0,0 +1,9 @@
+package test;
+
+public interface Calculatrice {
+
+	public int Addition(int a, int b) ;
+	public int Soustraction(int a, int b) ;
+	public int Multiplication(int a, int b) ;
+	public int Division(int a, int b) ;
+}
---
+++ Test_IC/src/test/Impl_CalculatriceTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Impl_CalculatriceTest {
+
+	@Test
+	public void testAddition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSoustraction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMultiplication() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDivision() {
+		fail("Not yet implemented");
+	}
+
+}
